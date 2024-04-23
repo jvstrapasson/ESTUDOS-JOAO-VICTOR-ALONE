4390c590f7c3b4d4f9b036d77bdb218f473c86d3
João Victor <gudistrapassonhtml@gmail.com>
2024-04-23 19:54:50 -0300

    created media archive

---
+++ media.py
@@ -0,0 +1,8 @@
+# Exercício para resolver médida de notas
+
+nota_1 = float (input("digite a nota 1"))
+nota_2 = float (input("digite a nota 2"))
+
+media = (nota_1 + nota_2)/ 2
+
+print(f" a nota final é: ", (media))
---
+++ teste.py
@@ -0,0 +1,6 @@
+nota_1 = float(input("Digite a nota 1:"))
+nota_2 = float(input("Digite a nota 2:"))
+
+media = (nota_1 + nota_2)/2
+
+print (f" A media é:{media}")
